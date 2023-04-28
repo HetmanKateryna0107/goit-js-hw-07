e1105a938933a96b8b4b52725183d421b3dc1d0d
HetmanKateryna0107 <125737708+HetmanKateryna0107@users.noreply.github.com>
2023-04-28 17:04:17 +0300

    task-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'
 
-console.log(galleryItems);
+
+const ulImg  = document.querySelector(".gallery");
+
+const createImgMarkp = ( array ) => {
+  const markup = array.map((el) => {
+    return`<li class="gallery__item">
+    <a class="gallery__link" 
+    href="${el.original}">
+       <img class="gallery__image" 
+       src="${el.preview}" 
+       alt="${el.description}"
+      
+    </a>
+ </li>`
+  }).join("")
+
+  ulImg.innerHTML=markup
+}
+
+createImgMarkp(galleryItems )
+
+lightbox = new SimpleLightbox('.gallery a', { 250});
+// const handleGaleryClick= (event)=>{
+//   event.preventDefault()
+//   const imgSrc = event.target.getAttribute("data-source")
+//   const instance = basicLightbox.create(`
+// 	<div><img src="${imgSrc}" width = "800" height="600"></div>`)
+
+// instance.show()}
